0425843645fb5b6331740b2b7094096fd65a706e
logikal16 <logikal16@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-18 01:04:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/pods/tags/1.4.2@87417 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajax/load.php
@@ -0,0 +1,46 @@
+<?php
+// Include the MySQL connection
+include(realpath('../../../../wp-config.php'));
+
+if ($_POST['auth'] != md5(AUTH_KEY))
+{
+    die('Error: Authentication failed');
+}
+
+// Get all Pod columns
+if ($id = (int) $_POST['id'])
+{
+    $result = pod_query("SELECT * FROM {$table_prefix}pod_types WHERE id = $id LIMIT 1");
+    $module = mysql_fetch_assoc($result);
+
+    $sql = "
+        SELECT
+            id, name, coltype, pickval, weight
+        FROM
+            {$table_prefix}pod_fields
+        WHERE
+            datatype = " . $module['id'] . "
+        ORDER BY
+            weight
+        ";
+
+    $result = pod_query($sql);
+    while ($row = mysql_fetch_assoc($result))
+    {
+        $fields[] = $row;
+    }
+
+    // Combine the fields into the $module array
+    $module['fields'] = $fields;
+
+    // Encode the array to JSON
+    echo json_encode($module);
+}
+// Get a single Pod column
+elseif ($field_id = (int) $_POST['col'])
+{
+    $result = pod_query("SELECT name, label, comment, coltype, pickval, sister_field_id, required FROM {$table_prefix}pod_fields WHERE id = $field_id LIMIT 1");
+    $row = mysql_fetch_assoc($result);
+    echo json_encode($row);
+}
+
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Pods ===
+Contributors: logikal16
+Donate link: http://wp-pods.googlecode.com
+Tags: pods, cms, content types, posts, pages, widgets, modules, datatypes
+Requires at least: 2.7
+Tested up to: 2.7
+Stable tag: trunk
+
+Pods is a CMS (Content Management System) plugin for WordPress.
+
+== Description ==
+
+Create more than just blog posts and pages. Add new content types with only the fields you need. Customize exactly how each content type's list and detail pages appear. Automatic pagination, filtering, public forms and more are possible with the Pods CMS plugin.
+
+Visit the [Pods homepage](http://pods.uproot.us)
+
+== Installation ==
+
+Visit the [Pods homepage](http://pods.uproot.us)
+
+== Frequently Asked Questions ==
+
+Visit the [Pods homepage](http://pods.uproot.us)
+
+== Screenshots ==
+
+Visit the [Pods homepage](http://pods.uproot.us)
