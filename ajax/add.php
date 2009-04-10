4d0e17ebbe7e909053d4786f317ffbb2fdf6d519
logikal16 <logikal16@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-10 01:03:33 +0000

    git-svn-id: https://plugins.svn.wordpress.org/pods/tags/1.5.7@109248 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajax/add.php
@@ -0,0 +1,157 @@
+<?php
+// Include the MySQL connection
+include(realpath('../../../../wp-config.php'));
+
+if ($_POST['auth'] != md5(AUTH_KEY))
+{
+    die('Error: Authentication failed');
+}
+
+foreach ($_POST as $key => $val)
+{
+    ${$key} = mysql_real_escape_string(stripslashes(trim($val)));
+}
+
+$name = pods_clean_name($name);
+
+/*
+==================================================
+Add new datatype
+==================================================
+*/
+if ('pod' == $type)
+{
+    if (!empty($name))
+    {
+        $sql = "SELECT id FROM @wp_pod_types WHERE name = '$name' LIMIT 1";
+        pod_query($sql, 'Cannot get pod type', 'Pod by this name already exists');
+
+        // Add list and detail template presets
+        $tpl_list = '<p><a href="{@detail_url}">{@name}</a></p>';
+        $tpl_detail = "<h2>{@name}</h2>\n{@body}";
+
+        $pod_id = pod_query("INSERT INTO @wp_pod_types (name, tpl_list, tpl_detail) VALUES ('$name', '$tpl_list', '$tpl_detail')", 'Cannot add new pod');
+        pod_query("CREATE TABLE @wp_pod_tbl_$name (id int unsigned auto_increment primary key, name varchar(128), body text)", 'Cannot add pod database table');
+        pod_query("INSERT INTO @wp_pod_fields (datatype, name, coltype, required, weight) VALUES ($pod_id, 'name', 'txt', 1, 0),($pod_id, 'body', 'desc', 0, 1)", 'Cannot add name and body columns');
+
+        die("$pod_id"); // return as string
+    }
+    die('Error: Enter a pod name!');
+}
+
+/*
+==================================================
+Add new page
+==================================================
+*/
+elseif ('page' == $type)
+{
+    if (!empty($uri))
+    {
+        $sql = "SELECT id FROM @wp_pod_pages WHERE uri = '$uri' LIMIT 1";
+        pod_query($sql, 'Cannot get PodPages', 'Page by this URI already exists');
+        $page_id = pod_query("INSERT INTO @wp_pod_pages (uri, phpcode) VALUES ('$uri', '$phpcode')", 'Cannot add new page');
+
+        die("$page_id"); // return as string
+    }
+    die('Error: Enter a page URI');
+}
+
+/*
+==================================================
+Add new helper
+==================================================
+*/
+elseif ('helper' == $type)
+{
+    if (!empty($name))
+    {
+        $sql = "SELECT id FROM @wp_pod_helpers WHERE name = '$name' LIMIT 1";
+        pod_query($sql, 'Cannot get helpers', 'helper by this name already exists');
+        $helper_id = pod_query("INSERT INTO @wp_pod_helpers (name, helper_type, phpcode) VALUES ('$name', '$helper_type', '$phpcode')", 'Cannot add new helper');
+
+        die("$helper_id"); // return as string
+    }
+    die('Error: Enter a helper name');
+}
+
+/*
+==================================================
+Add new menu item
+==================================================
+*/
+elseif ('menu' == $type)
+{
+    // get the "rgt" value of the parent
+    $result = pod_query("SELECT rgt FROM @wp_pod_menu WHERE id = $parent_menu_id LIMIT 1");
+    $row = mysql_fetch_assoc($result);
+    $rgt = $row['rgt'];
+
+    // Increase all "lft" values by 2 if > "rgt"
+    pod_query("UPDATE @wp_pod_menu SET lft = lft + 2 WHERE lft > $rgt");
+
+    // Increase all "rgt" values by 2 if >= "rgt"
+    pod_query("UPDATE @wp_pod_menu SET rgt = rgt + 2 WHERE rgt >= $rgt");
+
+    // Add new item: "lft" = rgt, "rgt" = rgt + 1
+    $lft = $rgt;
+    $rgt = ($rgt + 1);
+    $menu_id = pod_query("INSERT INTO @wp_pod_menu (uri, title, lft, rgt) VALUES ('$menu_uri', '$menu_title', $lft, $rgt)");
+
+    die("$menu_id"); // return as string
+}
+
+/*
+==================================================
+Add new column
+==================================================
+*/
+else
+{
+    if ('id' == $name || 'name' == $name || 'type' == $name || 'created' == $name || 'modified' == $name)
+    {
+        die("Error: $name is a reserved name");
+    }
+
+    $sql = "SELECT id FROM @wp_pod_fields WHERE datatype = $datatype AND name = '$name' LIMIT 1";
+    pod_query($sql, 'Cannot get fields', 'Column by this name already exists');
+
+    if ('slug' == $coltype)
+    {
+        $sql = "SELECT id FROM @wp_pod_fields WHERE datatype = $datatype AND coltype = 'slug' LIMIT 1";
+        pod_query($sql, 'Too many permalinks', 'This pod already has a permalink column');
+    }
+
+    // Sink the new column to the bottom of the list
+    $weight = 0;
+    $result = pod_query("SELECT weight FROM @wp_pod_fields WHERE datatype = $datatype ORDER BY weight DESC LIMIT 1");
+    if (0 < mysql_num_rows($result))
+    {
+        $row = mysql_fetch_assoc($result);
+        $weight = intval($row['weight']) + 1;
+    }
+
+    $sister_field_id = ('null' == $sister_field_id) ? 'NULL' : "$sister_field_id";
+    $field_id = pod_query("INSERT INTO @wp_pod_fields (datatype, name, label, helper, coltype, pickval, sister_field_id, required, `unique`, `multiple`, weight) VALUES ('$datatype', '$name', '$label', '$helper', '$coltype', '$pickval', $sister_field_id, '$required', '$unique', '$multiple', '$weight')", 'Cannot add new field');
+
+    if (empty($pickval))
+    {
+        $dbtypes = array(
+            'bool' => 'bool default 0',
+            'date' => 'datetime',
+            'num' => 'decimal(9,2)',
+            'txt' => 'varchar(128)',
+            'slug' => 'varchar(128)',
+            'file' => 'varchar(128)',
+            'code' => 'mediumtext',
+            'desc' => 'mediumtext'
+        );
+        $dbtype = $dbtypes[$coltype];
+        pod_query("ALTER TABLE @wp_pod_tbl_$dtname ADD COLUMN `$name` $dbtype", 'Cannot create new column');
+    }
+    else
+    {
+        pod_query("UPDATE @wp_pod_fields SET sister_field_id = '$field_id' WHERE id = $sister_field_id LIMIT 1", 'Cannot update sister field');
+    }
+}
+
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Pods ===
+Contributors: logikal16, sc0ttkclark
+Donate link: http://pods.uproot.us
+Tags: cms, pods, cck, drupal, joomla, content types, pages, datatypes
+Requires at least: 2.7
+Tested up to: 2.7.x
+Stable tag: trunk
+
+Pods is a CMS (Content Management System) plugin for WordPress.
+
+== Description ==
+
+Pods lets you create, manage, and display custom content types using WordPress. Like Drupal CCK, these content types can relate to one another, allowing for sites packed with interconnectedness. Automatic pagination, filtering, public forms, access control, menu editing and more are possible with the Pods CMS plugin. If you've been looking for a clean, lightweight alternative to Drupal/CCK or Joomla, then look no further.
+
+Visit the [Pods homepage](http://pods.uproot.us)
+
+== Installation ==
+
+Visit the [Pods homepage](http://pods.uproot.us)
+
+== Frequently Asked Questions ==
+
+Visit the [Pods homepage](http://pods.uproot.us)
+
+== Screenshots ==
+
+Visit the [Pods homepage](http://pods.uproot.us)
