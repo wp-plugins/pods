424d6806cac6ffa95420c03685bd936e46b12e28
logikal16 <logikal16@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-06 03:24:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/pods/tags/1.7.7pre3@170843 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+// Include the MySQL connection
+require_once(realpath(dirname(__FILE__) . '/../../../wp-config.php'));
+require_once(realpath(dirname(__FILE__) . '/init.php'));
+
+if (!pods_access('manage_settings'))
+{
+    die('Error: Access denied');
+}
+
+$result = pod_query("SHOW TABLES LIKE '@wp_pod%'");
+if (0 < mysql_num_rows($result))
+{
+    while ($row = mysql_fetch_array($result))
+    {
+        pod_query("DROP TABLE $row[0]");
+    }
+}
+pod_query("DELETE FROM @wp_options WHERE option_name LIKE 'pods%'");
