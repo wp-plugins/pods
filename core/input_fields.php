235000968980a84e26af6ade808e235200a1fc3f
logikal16 <logikal16@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-13 01:37:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/pods/tags/1.7.8pre3@172807 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/input_fields.php
@@ -0,0 +1,233 @@
+<?php
+$name = $field['name'];
+$label = $field['label'];
+$comment = $field['comment'];
+$coltype = $field['coltype'];
+$input_helper = $field['input_helper'];
+$hidden = empty($field['hidden']) ? '' : ' hidden';
+$value = is_array($this->data[$name]) ? $this->data[$name] : stripslashes($this->data[$name]);
+?>
+    <div class="leftside <?php echo $name . $hidden; ?>">
+        <?php echo $label; ?>
+<?php
+if (!empty($comment))
+{
+?>
+        <div class="comment"><?php echo $comment; ?></div>
+<?php
+}
+?>
+    </div>
+    <div class="rightside <?php echo $name . $hidden; ?>">
+<?php
+/*
+==================================================
+Generate the input helper
+==================================================
+*/
+if (!empty($input_helper))
+{
+    eval("?>$input_helper");
+}
+
+/*
+==================================================
+Boolean checkbox
+==================================================
+*/
+elseif ('bool' == $coltype)
+{
+    $value = empty($value) ? '' : ' checked';
+?>
+    <input type="checkbox" class="form bool <?php echo $name; ?>"<?php echo $value; ?> />
+<?php
+}
+
+/*
+==================================================
+Date picker
+==================================================
+*/
+elseif ('date' == $coltype)
+{
+    $value = empty($value) ? date("Y-m-d H:i:s") : $value;
+?>
+    <input type="text" class="form date <?php echo $name; ?>" value="<?php echo $value; ?>" />
+<?php
+}
+
+/*
+==================================================
+Standard text box
+==================================================
+*/
+elseif ('num' == $coltype || 'txt' == $coltype || 'slug' == $coltype)
+{
+?>
+    <input type="text" class="form <?php echo $coltype . ' ' . $name; ?>" value="<?php echo htmlspecialchars($value); ?>" />
+<?php
+}
+
+/*
+==================================================
+Textarea box
+==================================================
+*/
+elseif ('desc' == $coltype)
+{
+?>
+    <textarea class="form desc <?php echo $name; ?>" id="desc-<?php echo $name; ?>"><?php echo $value; ?></textarea>
+<?php
+}
+
+/*
+==================================================
+Textarea box (no WYSIWYG)
+==================================================
+*/
+elseif ('code' == $coltype)
+{
+?>
+    <textarea class="form code <?php echo $name; ?>" id="code-<?php echo $name; ?>"><?php echo $value; ?></textarea>
+<?php
+}
+
+/*
+==================================================
+File upload
+==================================================
+*/
+elseif ('file' == $coltype)
+{
+    require_once(realpath(ABSPATH . '/wp-admin/includes/template.php'));
+?>
+<script type="text/javascript">
+jQuery(function() {
+    swfu_<?php echo $name; ?> = new SWFUpload({
+        button_text: '<span class="button">Select + Upload</span>',
+        button_text_style: '.button { text-align:center; color:#464646; font-size:11px; font-family:"Lucida Grande",Verdana,Arial,"Bitstream Vera Sans",sans-serif; }',
+        button_width: "132",
+        button_height: "24",
+        button_text_top_padding: 3,
+        button_image_url: "<?php echo WP_INC_URL; ?>/images/upload.png",
+        button_placeholder_id: "btn_<?php echo $name; ?>",
+        button_cursor: SWFUpload.CURSOR.HAND,
+        button_window_mode: SWFUpload.WINDOW_MODE.TRANSPARENT,
+        upload_url: "<?php echo PODS_URL; ?>/ajax/misc.php",
+        flash_url: "<?php echo WP_INC_URL; ?>/js/swfupload/swfupload.swf",
+        file_types: "*.*",
+        file_size_limit: "<?php echo wp_max_upload_size(); ?>",
+        post_params: {"action": "wp_handle_upload"},
+        file_dialog_complete_handler: function(num_files, num_queued_files, total_queued_files) {
+            this.startUpload();
+        },
+        file_queued_handler: function(file) {
+            jQuery(".rightside.<?php echo $name; ?> .form").append('<div id="' + file.id + '">' + file.name + '<div class="pods-progress"><div class="pods-bar"></div></div></div>');
+        },
+        upload_progress_handler: function(file, bytes_complete, bytes_total) {
+            var percent = Math.ceil(100 * (bytes_complete / bytes_total));
+            jQuery("#"+file.id+" .pods-bar").css("width", percent + "%");
+        },
+        upload_success_handler: function(file, server_data, response) {
+            jQuery("#"+file.id+" .pods-progress").remove();
+
+            if ("Error" == server_data.substr(0, 5)) {
+                server_data = server_data.substr(7);
+                jQuery("#"+file.id).append(server_data);
+            }
+            else {
+                jQuery("#"+file.id).prepend('<div class="btn dropme"></div>');
+                jQuery("#"+file.id).attr("class", "success");
+                jQuery("#"+file.id).attr("id", server_data);
+            }
+        },
+        upload_complete_handler: function(file) {
+            this.startUpload();
+        }
+    });
+});
+</script>
+    <input type="button" id="btn_<?php echo $name; ?>" value="swfupload not loaded" />
+    <input type="button" class="button" value="Browse Server" onclick="active_file = '<?php echo $name; ?>'; fileBrowser()" />
+    <div class="form file <?php echo $name; ?>">
+<?php
+    // Retrieve uploaded files
+    $field_id = $field['id'];
+    $pod_id = $this->get_pod_id();
+    $sql = "
+    SELECT
+        p.ID, p.guid
+    FROM
+        @wp_pod_rel r
+    INNER JOIN
+        @wp_posts p ON p.post_type = 'attachment' AND p.ID = r.tbl_row_id
+    WHERE
+        r.pod_id = '$pod_id' AND r.field_id = '$field_id'
+    ";
+    $result = pod_query($sql);
+    while ($row = mysql_fetch_assoc($result))
+    {
+        $filepath = $row['guid'];
+        $filename = substr($filepath, strrpos($filepath, '/') + 1);
+?>
+        <div id="<?php echo $row['ID']; ?>" class="success">
+            <div class="btn dropme"></div><?php echo $filename; ?>
+        </div>
+<?php
+    }
+?>
+    </div>
+<?php
+}
+
+/*
+==================================================
+PICK select box
+==================================================
+*/
+else
+{
+    // Multi-select
+    if (1 == $field['multiple'])
+    {
+?>
+    <div class="form pick <?php echo $name; ?>">
+<?php
+        if (!empty($value))
+        {
+            foreach ($value as $key => $val)
+            {
+                $active = empty($val['active']) ? '' : ' active';
+?>
+        <div class="option<?php echo $active; ?>" value="<?php echo $val['id']; ?>"><?php echo $val['name']; ?></div>
+<?php
+            }
+        }
+?>
+    </div>
+<?php
+    }
+    else
+    {
+?>
+    <select class="form pick1 <?php echo $name; ?>">
+        <option value="">-- Select one --</option>
+<?php
+        if (!empty($value))
+        {
+            foreach ($value as $key => $val)
+            {
+                $selected = empty($val['active']) ? '' : ' selected';
+?>
+        <option value="<?php echo $val['id']; ?>"<?php echo $selected; ?>><?php echo $val['name']; ?></option>
+<?php
+            }
+        }
+?>
+    </select>
+<?php
+    }
+}
+?>
+    </div>
+    <div class="clear<?php echo $hidden; ?>"></div>
---
+++ core/manage_content.php
@@ -0,0 +1,360 @@
+<?php
+$result = pod_query("SELECT id, name FROM @wp_pod_types ORDER BY name ASC");
+while ($row = mysql_fetch_assoc($result))
+{
+    $datatypes[$row['id']] = $row['name'];
+}
+
+$add_or_edit = 'edit';
+$page = explode('-', $_GET['page']);
+if ('pod' == $page[0])
+{
+    $dtname = strtolower($page[1]);
+    $datatype = array_search($dtname, $datatypes);
+    $add_or_edit = 'add';
+}
+else
+{
+    $dtname = isset($page[2]) ? $page[2] : $_GET['pod'];
+}
+?>
+
+<!--
+==================================================
+Begin javascript code
+==================================================
+-->
+<link rel="stylesheet" type="text/css" href="<?php echo PODS_URL; ?>/style.css" />
+<script type="text/javascript" src="<?php echo PODS_URL; ?>/js/jqmodal.js"></script>
+<script type="text/javascript" src="<?php echo PODS_URL; ?>/js/date_input.js"></script>
+<script type="text/javascript" src="<?php echo PODS_URL; ?>/js/nicEdit.js"></script>
+<script type="text/javascript">
+var datatype;
+var active_file;
+var add_or_edit = '<?php echo $add_or_edit; ?>';
+var auth = '<?php echo md5(AUTH_KEY); ?>';
+
+jQuery(function() {
+    jQuery(".navTab").click(function() {
+        jQuery(".navTab").removeClass("active");
+        jQuery(this).addClass("active");
+        var activeArea = jQuery(this).attr("rel");
+        jQuery(".area").hide();
+        jQuery("#"+activeArea).show();
+    });
+    if ('add' == add_or_edit) {
+        jQuery(".navTab").click();
+        jQuery("#editTitle").html("Add new <?php echo $dtname; ?>");
+    }
+
+    jQuery(".file .btn.dropme").live("click", function() {
+        jQuery(this).parent().remove();
+    });
+
+    jQuery(".file_match").live("click", function() {
+        var file_id = jQuery(this).attr("rel");
+        var file_name = jQuery(this).html();
+        jQuery(".rightside." + active_file + " .form").append('<div id="' + file_id + '" class="success"><div class="btn dropme"></div>' + file_name + '</div>');
+        jQuery("#dialog").jqmHide();
+    });
+
+    jQuery("#browseTable tr:odd").addClass("zebra");
+    jQuery("#dialog").jqm();
+});
+
+function editItem(datatype, pod_id) {
+    jQuery(".navTab").click();
+    jQuery("#editTitle").html("Edit "+datatype);
+    showform(datatype, pod_id);
+}
+
+function dropItem(pod_id) {
+    if (confirm("Do you really want to drop this item?")) {
+        jQuery.ajax({
+            type: "post",
+            url: "<?php echo PODS_URL; ?>/ajax/drop.php",
+            data: "auth="+auth+"&pod_id="+pod_id,
+            success: function(msg) {
+                if ("Error" == msg.substr(0, 5)) {
+                    alert(msg);
+                }
+                else {
+                    jQuery("#browseTable tr#row"+pod_id).css("background", "red");
+                    jQuery("#browseTable tr#row"+pod_id).fadeOut("slow");
+                }
+            }
+        });
+    }
+}
+
+function saveForm() {
+    for (i = 0; i < elements.length; i++) {
+        nicEditors.findEditor(elements[i].id).saveContent();
+    }
+
+    var data = new Array();
+
+    var i = 0;
+    jQuery(".form").each(function() {
+        var theval = "";
+        var classname = jQuery(this).attr("class").split(" ");
+        if ("pick" == classname[1]) {
+            jQuery("." + classname[2] + " .active").each(function() {
+                theval += jQuery(this).attr("value") + ",";
+            });
+            theval = theval.slice(0, -1);
+        }
+        else if ("file" == classname[1]) {
+            jQuery("." + classname[2] + " > div.success").each(function() {
+                theval += jQuery(this).attr("id") + ",";
+            });
+            theval = theval.slice(0, -1);
+        }
+        else if ("bool" == classname[1]) {
+            theval = (true == jQuery(this).is(":checked")) ? 1 : 0;
+        }
+        else {
+            theval = jQuery(this).val();
+        }
+        data[i] = classname[2] + "=" + encodeURIComponent(theval);
+        i++;
+    });
+
+    jQuery.ajax({
+        type: "post",
+        url: "<?php echo PODS_URL; ?>/ajax/showform.php",
+        data: data.join("&"),
+        success: function(msg) {
+            if ("Error" == msg.substr(0, 5)) {
+                alert(msg);
+            }
+            else {
+                window.location="";
+            }
+        }
+    });
+    return false;
+}
+
+function showform(dt, pod_id) {
+    if ("" == dt) {
+        return false;
+    }
+    datatype = dt;
+    jQuery(".option").unbind("click");
+    jQuery.ajax({
+        type: "post",
+        url: "<?php echo PODS_URL; ?>/ajax/showform.php",
+        data: "pod_id="+pod_id+"&datatype="+datatype,
+        success: function(msg) {
+            if ("Error" == msg.substr(0, 5)) {
+                alert(msg);
+            }
+            else {
+                jQuery(".pod_form").hide();
+                jQuery(".pod_form").html(msg);
+                jQuery(".pod_form").show();
+                jQuery(".option").click(function() {
+                    jQuery(this).toggleClass("active");
+                });
+
+                elements = jQuery(".desc");
+                var config = {
+                    iconsPath : "<?php echo PODS_URL; ?>/images/nicEditorIcons.gif",
+                    buttonList : ['bold','italic','underline','fontFormat','left','center','right','justify','ol','ul','indent','outdent','image','link','unlink','xhtml']
+                };
+
+                for (i = 0; i < elements.length; i++) {
+                    new nicEditor(config).panelInstance(elements[i].id);
+                }
+                jQuery("input.date").date_input();
+            }
+        }
+    });
+}
+
+function fileBrowser() {
+    jQuery("#dialog").jqmShow();
+    jQuery(".filebox").html("Loading...");
+    var search = jQuery("#file_search").val();
+    jQuery.ajax({
+        type: "post",
+        url: "<?php echo PODS_URL; ?>/ajax/filetree.php",
+        data: "search="+encodeURIComponent(search),
+        success: function(msg) {
+            jQuery(".filebox").html(msg);
+        }
+    });
+}
+</script>
+
+<div class="jqmWindow" id="dialog">
+    <input type="text" id="file_search" value="" />
+    <input type="button" class="button" value="Narrow results" onclick="fileBrowser()" />
+    <div class="filebox"></div>
+</div>
+
+<!--
+==================================================
+Begin tabbed navigation
+==================================================
+-->
+<div id="nav">
+    <div class="navTab active" rel="browseArea"><a href="javascript:;">Browse</a></div>
+    <div class="navTab" rel="editArea"><a href="javascript:;">Edit</a></div>
+    <div class="clear"><!--clear--></div>
+</div>
+
+<!--
+==================================================
+Begin browse area
+==================================================
+-->
+<div id="browseArea" class="area">
+<?php
+$Record = new Pod();
+$Record->page = empty($_GET['pg']) ? 1 : $_GET['pg'];
+$limit = (15 * ($Record->page - 1)) . ',15';
+$Record->type = '';
+
+$where[] = 1;
+
+if (!empty($dtname))
+{
+    $where[] = "t.name = '" . mysql_real_escape_string(trim($dtname)) . "'";
+}
+
+if (!empty($_GET['keywords']))
+{
+    $where[] = "p.name LIKE '%" . mysql_real_escape_string(trim($_GET['keywords'])) . "%'";
+}
+
+$orderby = 'modified desc';
+foreach ($_GET as $key => $val)
+{
+    if ('orderby' != $key)
+    {
+        $get_vals[$key] = "$key=$val";
+    }
+    else
+    {
+        $orderby = $_GET[$key];
+    }
+}
+
+$where = implode(' AND ', $where);
+
+$sql = "
+SELECT
+    SQL_CALC_FOUND_ROWS
+    p.id, p.name, p.datatype, t.name AS dtname, p.modified
+FROM
+    @wp_pod p
+INNER JOIN
+    @wp_pod_types t ON t.id = p.datatype
+WHERE
+    $where
+ORDER BY
+    $orderby, name
+LIMIT
+    $limit
+";
+$result = pod_query($sql);
+
+$Record->total_rows = pod_query("SELECT FOUND_ROWS()");
+?>
+    <div id="filterForm">
+        <form method="get">
+<?php
+if ('pods-browse' == $_GET['page'])
+{
+?>
+            <select class="pick_module" name="pod">
+                <option value="">-- All pods --</option>
+<?php
+    foreach ($datatypes as $key => $name)
+    {
+        $selected = ($name == $dtname) ? ' selected' : '';
+?>
+                <option value="<?php echo $name; ?>"<?php echo $selected; ?>><?php echo $name; ?></option>
+<?php
+    }
+?>
+            </select>
+<?php
+}
+?>
+            <input type="text" id="column_name" name="keywords" />
+            <input type="hidden" name="page" value="<?php echo $_GET['page']; ?>" />
+            <input type="button" class="button" value="Narrow results" onclick="this.form.submit()" />
+        </form>
+    </div>
+    <div class="clear"><!--clear--></div>
+<?php
+/*
+==================================================
+Listing
+==================================================
+*/
+$get_vals = implode('&', $get_vals);
+$order = array('name' => 'name', 'dtname' => 'dtname', 'modified' => 'modified');
+if (!empty($orderby))
+{
+    if ('desc' != substr($orderby, -4))
+    {
+        $order[$orderby] = "$orderby+desc";
+    }
+}
+?>
+    <table id="browseTable" cellpadding="0" cellspacing="0">
+        <tr>
+            <th></th>
+            <th><a href="?<?php echo $get_vals . '&orderby=' . $order['name']; ?>">Name</a></th>
+            <th><a href="?<?php echo $get_vals . '&orderby=' . $order['dtname']; ?>">Pod</a></th>
+            <th><a href="?<?php echo $get_vals . '&orderby=' . $order['modified']; ?>">Modified</a></th>
+            <th></th>
+        </tr>
+<?php
+while ($row = mysql_fetch_assoc($result))
+{
+?>
+        <tr id="row<?php echo $row['id']; ?>">
+            <td width="20">
+                <div class="btn editme" onclick="editItem('<?php echo $row['dtname']; ?>', <?php echo $row['id']; ?>)"></div>
+            </td>
+            <td><?php echo htmlspecialchars($row['name']); ?></td>
+            <td><?php echo $datatypes[$row['datatype']]; ?></td>
+            <td><?php echo date("m/d/Y g:i A", strtotime($row['modified'])); ?></td>
+            <td width="20"><div class="btn dropme" onclick="dropItem(<?php echo $row['id']; ?>)"></div></td>
+        </tr>
+<?php
+}
+?>
+    </table>
+
+    <?php echo $Record->getPagination(); ?>
+
+    <div class="clear"><!--clear--></div>
+</div>
+
+<!--
+==================================================
+Begin edit area
+==================================================
+-->
+<div id="editArea" class="area hidden">
+    <div id="icon-plugins" class="icon32" style="margin:0 6px 0 0"><br /></div>
+    <h2 class="title" id="editTitle">Please select an item</h2>
+    <div class="clear"><!--clear--></div>
+
+    <div class="pod_form">
+<?php
+if ('add' == $add_or_edit)
+{
+?>
+        <script type="text/javascript">showform('<?php echo $dtname; ?>')</script>
+<?php
+}
+?>
+    </div>
+</div>
