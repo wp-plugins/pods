aad81a6e99bd8c64c84155efbad8987f095dea75
sc0ttkclark <sc0ttkclark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-02 12:21:52 +0000

    Pods 1.12-rc-7 w/ WP 3.3 support
    git-svn-id: https://plugins.svn.wordpress.org/pods/trunk@470196 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pods-ui.php
+++ pods-ui.php
@@ -1582,19 +1582,27 @@ function pods_ui_verify_access  ($object,$access,$what)
                 $okay = false;
                 foreach($match as $the_field=>$the_match)
                 {
-                    if($object->get_field($the_field)==$the_match)
-                    {
-                        $okay = true;
+                    $value = $object->get_field($the_field);
+                    if (is_array($value)) {
+                        if (in_array($the_match, $value))
+                            $okay = true;
                     }
+                    elseif($value==$the_match)
+                        $okay = true;
                 }
                 if($okay===false)
                 {
                     return false;
                 }
             }
-            elseif($object->get_field($field)!=$match)
-            {
-                return false;
+            else {
+                $value = $object->get_field($field);
+                if (is_array($value)) {
+                    if (!in_array($match, $value))
+                        return false;
+                }
+                elseif($value!=$match)
+                    return false;
             }
         }
     }
