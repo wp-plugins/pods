d2aafd4d5bdbb30a36ebfb3e0a5618f8f6ec1fc1
sc0ttkclark <sc0ttkclark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-21 23:22:24 +0000

    Pods 2.0 goes live - on the air http://youtu.be/SutU2B64eoY
    git-svn-id: https://plugins.svn.wordpress.org/pods/trunk@602158 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- init.php
+++ init.php
@@ -2,16 +2,17 @@
 /*
 Plugin Name: Pods Framework
 Plugin URI: http://podsframework.org/
-Description: Pods is a framework for creating, managing, and deploying customized content types.
-Version: 1.14.4
+Description: Create / Manage / Develop / Extend content types: Posts, Pages, Media, Custom Post Types, Categories, Tags, Custom Taxonomy, Comments, Users, Custom Content Types, and Custom Tables
+Version: 2.0.0 RC 1
 Author: Pods Framework Team
 Author URI: http://podsframework.org/about/
-License: GPLv2 or later
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+Copyright 2009-2012  The Pods Framework Team  (email : contact@podsframework.org)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,66 +21,92 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// Pods 2.0 beta tester compatibility
-if ( !defined( 'PODS_VERSION' ) ) {
-    define( 'PODS_VERSION', '1.14.4' );
+/**
+ * @package Pods\Global
+ */
+
+// Prevent conflicts with Pods 1.x
+if ( !defined( 'PODS_VERSION' ) && !defined( 'PODS_DIR' ) ) {
+    define( 'PODS_VERSION', '2.0.0-rc-1' );
 
     if ( !defined( 'PODS_WP_VERSION_MINIMUM' ) )
-        define( 'PODS_WP_VERSION_MINIMUM', '3.1' );
+        define( 'PODS_WP_VERSION_MINIMUM', '3.4' );
     if ( !defined( 'PODS_PHP_VERSION_MINIMUM' ) )
         define( 'PODS_PHP_VERSION_MINIMUM', '5.2.4' );
     if ( !defined( 'PODS_MYSQL_VERSION_MINIMUM' ) )
         define( 'PODS_MYSQL_VERSION_MINIMUM', '5.0' );
 
-    define( 'PODS_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) ); // non-trailing slash being deprecated in 2.0
-    define( 'PODS_DIR', rtrim( plugin_dir_path( __FILE__ ), '/' ) ); // non-trailing slash being deprecated in 2.0
-    define( 'WP_INC_URL', rtrim( includes_url(), '/' ) ); // non-trailing slash being deprecated in 2.0
+    define( 'PODS_SLUG', plugin_basename( __FILE__ ) );
+    define( 'PODS_URL', plugin_dir_url( __FILE__ ) );
+    define( 'PODS_DIR', plugin_dir_path( __FILE__ ) );
 
-    require_once( PODS_DIR . '/functions.php' );
+    require_once( PODS_DIR . 'functions.php' );
 
-    require_once( PODS_DIR . '/classes/PodInit.php' );
+    $update = admin_url( 'update.php' );
+    $update = str_replace( get_bloginfo( 'wpurl' ), '', $update );
 
-    require_once( PODS_DIR . '/classes/Pod.php' );
-    require_once( PODS_DIR . '/classes/PodAPI.php' );
+    $update_network = network_admin_url( 'update.php' );
+    $update_network = str_replace( get_bloginfo( 'wpurl' ), '', $update_network );
 
-    require_once( PODS_DIR . '/classes/PodCache.php' );
+    if ( is_admin() &&
+         ( isset( $_GET[ 'pods_force_refresh' ] ) ||
+           ( 'update-selected' == pods_var( 'action' ) &&
+             ( false !== strpos( $_SERVER[ 'REQUEST_URI' ], $update ) ||
+               false !== strpos( $_SERVER[ 'REQUEST_URI' ], $update_network ) ) ) ) ) {
+        // GitHub Plugin Updater
+        // https://github.com/jkudish/WordPress-GitHub-Plugin-Updater
+        require_once( PODS_DIR . 'updater.php' );
 
-    require_once( PODS_DIR . '/pods-ui.php' );
+        $version = PODS_VERSION;
 
-    global $pods_cache, $cache, $pods_init;
+        if ( isset( $_GET[ 'pods_force_refresh' ] ) )
+            $version = '0.1';
+
+        if ( 'update-selected' == pods_var( 'action' ) && ( false !== strpos( $_SERVER[ 'REQUEST_URI' ], $update ) || false !== strpos( $_SERVER[ 'REQUEST_URI' ], $update_network ) ) )
+            $version = '0.1';
+
+        $user = 'pods-framework';
+        $repo = 'pods';
+        $branch = '2.0';
+        $config = array(
+            'slug' => PODS_SLUG, // this is the slug of your plugin
+            'proper_folder_name' => dirname( PODS_SLUG ), // this is the name of the folder your plugin lives in
+            'api_url' => 'https://api.github.com/repos/' . $user . '/' . $repo, // the github API url of your github repo
+            'raw_url' => 'https://raw.github.com/' . $user . '/' . $repo . '/' . $branch, // the github raw url of your github repo
+            'github_url' => 'https://github.com/' . $user . '/' . $repo, // the github url of your github repo
+            'zip_url' => 'https://github.com/' . $user . '/' . $repo . '/zipball/' . $branch, // the zip url of the github repo
+            'sslverify' => false, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+            'requires' => '3.4', // which version of WordPress does your plugin require?
+            'tested' => '3.4.1', // which version of WordPress is your plugin tested up to?
+            'version' => $version
+        );
+
+        new WPGitHubUpdater( $config );
+    }
+
+    global $pods, $pods_init;
+
     if ( false !== pods_compatible() && ( !defined( 'SHORTINIT' ) || !SHORTINIT ) ) {
-        // JSON support
-        if ( !function_exists( 'json_encode' ) ) {
-            require_once( ABSPATH . '/wp-includes/js/tinymce/plugins/spellchecker/classes/utils/JSON.php' );
+        if ( !defined( 'PODS_DEPRECATED' ) || PODS_DEPRECATED )
+            require_once( PODS_DIR . 'deprecated/deprecated.php' );
 
-            function json_encode( $str ) {
-                $json = new Moxiecode_JSON();
-                return $json->encode( $str );
-            }
-
-            function json_decode( $str ) {
-                $json = new Moxiecode_JSON();
-                return $json->decode( $str );
-            }
-        }
-
-        $pods_cache = PodCache::instance();
-        $cache = &$pods_cache; // DEPRECATED IN 2.0
-        $pods_init = new PodInit();
+        $pods_init = pods_init();
     }
 }
 else {
-    function pods_deactivate_1_x() {
-        if ( !function_exists( 'deactivate_plugins' ) )
-            include_once ABSPATH . '/wp-admin/includes/plugin.php';
+    function pods_deactivate_1_x () {
+        if ( defined( 'PODS_VERSION' ) && defined( 'PODS_DIR' ) && file_exists( untrailingslashit( PODS_DIR ) . '/init.php' ) ) {
+            if ( !function_exists( 'deactivate_plugins' ) )
+                include_once ABSPATH . '/wp-admin/includes/plugin.php';
 
-        deactivate_plugins( __FILE__ );
+            deactivate_plugins( untrailingslashit( PODS_DIR ) . '/init.php' );
 
-        // next refresh will load 2.0
-        return;
+            // next refresh will load 2.0
+            return;
+        }
     }
 
     add_action( 'init', 'pods_deactivate_1_x' );
